175d9882808769822f8fa9a064e6d9ce77239293
ZacharyW-d <83637256+ZacharyW-d@users.noreply.github.com>
2021-05-04 16:25:38 +0800

    Add files via upload

---
+++ currency_formatter.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Author: Zachary Wu
+# Edit time: 05-03-2021
+# status: partial complete
+
+###
+# realize currency_formatter which takes in series of string data and corresponding denotation type (euro, USD, etc.)
+# and convert into integer, float or other denotation type 
+###
+
+import pandas as pd
+
+
+class Currency:
+    def __init__(self, data, ctype):
+        self.data = data
+        self.ctype = ctype
+    
+    def to_int(self):
+        if self.ctype == 'euro':
+            return self.euro_to_int(self.data)
+        
+        elif self.nation == 'usd':
+            return self.usd_to_int(self.data)
+        
+    def euro_to_int(self, data):
+        data = data.str.replace('.', '')
+        data = data.str.replace(',', '.')
+        data = pd.to_numeric(data)
+        
+        return data
+        
+    def usd_to_int(self, data):
+        return 
+
+
+# In[ ]:
+
+
+
+
